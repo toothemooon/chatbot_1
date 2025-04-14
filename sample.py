7e2812869c28c88c39c4d4211c937acd93be5d49
toothemooon <35186738+toothemooon@users.noreply.github.com>
2025-04-14 19:41:54 +0900

    hey

---
+++ sample.py
@@ -0,0 +1,11 @@
+print('a', 'b', sep=' $ ')
+print('Does this mean that I can use', end=' different ')
+print('lines by using the end argument')
+
+coins = [1, 5, 10, 50, 100, 500]
+print(*coins)
+
+list_A = [1, 2, 3]
+for one_thing in list_A:
+    print(one_thing + 1)
+
