1059e46509aad5383e75f36fd516777e2c87da22
toothemooon <35186738+toothemooon@users.noreply.github.com>
2025-04-15 18:27:42 +0900

    ——

---
+++ main_module.py
@@ -0,0 +1,5 @@
+# main_module.py
+
+import test_module
+
+print('Hello from main_module.py, my __name__ is', __name__)
---
+++ test_module.py
@@ -0,0 +1,7 @@
+# test_module.py
+
+print('Hello from test_module.py, my __name__ is', __name__)
+
+if __name__ == "__main__":
+    # only runs when executed directly
+    print("Hello")
